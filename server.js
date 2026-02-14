492a42afeda3f1c9651673e76d8ad6851a0b7fec
almondp040 <almondpaschal@gmail.com>
2026-02-13 23:48:56 -0500

    installed prettier and configured it

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 // Serve static files (HTML, CSS, images, etc.) from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-
