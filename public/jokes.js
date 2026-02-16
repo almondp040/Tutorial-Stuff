06e2cf2aebadf4228f9de45db7a9f3c62a009669
almondp040 <almondpaschal@gmail.com>
2026-02-15 19:56:32 -0500

    added a bit more jokes logic

--- public/jokes.js
+++ public/jokes.js
@@ -1,8 +1,10 @@
+// Jokes Page Logic:
 import { grabJokeFromAPI } from "./jokesAPI.js";
 
 // Contain the logic to add our API data to the jokes page
 const jokesParagraph = document.getElementById("jokesParagraph");
 const jokesPunchline = document.getElementById("jokesPunchline");
+const newJokeButton = document.getElementById("newJokeButton");
 
 // Set the inner text to the data coming from the api:
 const populateJokes = () => {
@@ -16,11 +18,24 @@ const populateJokes = () => {
   });
 };
 
+//Can be used to populate the jokes on page load:
 populateJokes();
 
 
 //Upon clicking on the setup we want to change the display to be visible
 jokesParagraph.addEventListener("click", () => {
   //change the color to black to make the punchline visible
-  jokesPunchline.style.color = "black";
+  jokesPunchline.style.visibility = "visible";
+
+  //show button here
+  newJokeButton.style.visibility = "visible";
+});
+
+
+// When clicking the new joke button we want to either refresh the page or show new jokes
+newJokeButton.addEventListener("click", () => {
+  //Pull a new joke from the API: 
+  populateJokes();
+  //Make the punchline invisible again:
+  jokesPunchline.style.visibility = "hidden";
 });
